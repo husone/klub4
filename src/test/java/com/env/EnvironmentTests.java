91b1159e4269d63fa4be51879c766d53f471f130
quangpao <ddquangbao@gmail.com>
2022-10-26 16:49:46 +0700

    change

--- src/test/java/com/env/EnvironmentTests.java
+++ src/test/java/com/env/EnvironmentTests.java
@@ -5,15 +5,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import io.github.cdimascio.dotenv.Dotenv;
+import database.readENV;
+
 
 public class EnvironmentTests {
     
     @Test
     @DisplayName("Test environment")
     void testEnvironment() {
-        Dotenv dotenv = Dotenv.load();
-        String email_username = dotenv.get("EMAIL_USERNAME");
+        String email_username = readENV.get("EMAIL_USERNAME");
         assertEquals("sonsieusen1@gmail.com", email_username);
     }
 
