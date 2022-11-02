9bea634e64135adc48011d2d904d569505637237
quangpao <ddquangbao@gmail.com>
2022-11-02 21:32:04 +0700

    Sidebar-user redirect href link to servlet

--- src/main/java/database/DatabaseInfor.java
+++ src/main/java/database/DatabaseInfor.java
@@ -4,12 +4,6 @@
  * and open the template in the editor.
  */
 package database;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Scanner;
-
 /**
  *
  * @author sonsi
@@ -18,28 +12,7 @@ public interface DatabaseInfor {
 
 
     public String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    public String url = get("DTBURL");
-    public String user = get("DTBUSERNAME");
-    public String pass = get("DTBPASSWORD");
-
-    private static String get(String key) {
-        try {
-            File myObj = new File(".env");
-            Scanner myReader = new Scanner(myObj);
-            HashMap<String, String> env = new HashMap<String, String>();
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                data = data.replace(" ", "");
-                String[] arr = data.split("=\"");
-                env.put(arr[0], arr[1].replace("\"", ""));
-                // System.out.println(arr[1].replace("\"", ""));
-            }
-            myReader.close();
-            return env.get(key);
-        } catch (FileNotFoundException e ) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-        return "";
-    }
+    public String url = "jdbc:sqlserver://quangpao.database.windows.net:1433;database=ClubDB;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+    public String user = "CloudSA8b57ce08";
+    public String pass = "BlQpro123";
 }
