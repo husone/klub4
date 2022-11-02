80e9d82412143c825f59046ec1c7b56a1dfd22a2
quangpao <ddquangbao@gmail.com>
2022-11-02 21:35:23 +0700

    database connect to Bao azure sql database

--- src/main/java/database/DatabaseInfor.java
+++ src/main/java/database/DatabaseInfor.java
@@ -14,5 +14,5 @@ public interface DatabaseInfor {
     public String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
     public String url = "jdbc:sqlserver://quangpao.database.windows.net:1433;database=ClubDB;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
     public String user = "CloudSA8b57ce08";
-    public String pass = "BlQpro123";
+    public String pass = "123456Aa";
 }
